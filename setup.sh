c839fdd6699ec2fc085ae617dabc3ba3e0519288
Levi McClenny <levimcclenny@tamu.edu>
2021-02-20 20:30:01 -0600

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# necessary commands for setting up coolc and spim commands
+
+# Create a simlink - ln -s absolute/path/to/cool ~/cool
+ln -s /home/levi/Repositories/cool ~/cool
+
+# add commands in /bin to $PATH - PATH= absolute/path/to/cool/bin/:$PATH
+
+PATH=/home/levi/Repositories/cool/bin/:$PATH
